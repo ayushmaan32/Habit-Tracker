ec98ed3ec67da766c9a168da7a49d252a6c71b8d
ayushmaan <ayushmaanroy.12@gmail.com>
2023-04-28 18:08:46 +0530

    setup the express router

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 5000;
 
 const app = express();
 
+//
+app.use("/", require("./routes"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running server: ${port}`);
