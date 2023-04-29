b07c9c15a838d3b58ebbba1fab5da71debd1b04d
ayushmaan <ayushmaanroy.12@gmail.com>
2023-04-29 13:00:02 +0530

    linking our MongoDB using mongoose files for Pages

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const app = express();
 
 const expressLayouts = require("express-ejs-layouts");
 
+const db = require("./config/mongoose");
+
+app.use(express.static("./assets"));
+
 app.use(expressLayouts);
+
+// extract styles and scripts from sub Pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //use express router
 app.use("/", require("./routes"));
